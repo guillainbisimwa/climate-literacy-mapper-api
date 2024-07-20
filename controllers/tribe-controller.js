42911f2604f8099b60bfe37d9dd90038504c7848
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-07-20 18:32:54 +0200

    Delete and update tribe

--- controllers/tribe-controller.js
+++ controllers/tribe-controller.js
@@ -35,3 +35,53 @@ exports.findOne = async (req, res) => {
  * @param {*} res
  * @returns  { message: String , avec: Array }
  */
+
+exports.create = async (req, res) => {
+  const newTribe = new Tribe(req.body);
+  try {
+    const savedTribe = await newTribe.save();
+    return res
+      .status(201)
+      .json({ message: "Tribe saved successfully", avec: savedTribe });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: "Tribe not saved!", details: err.message });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const foundTribe = await Tribe.findOne({ _id: req.params.id });
+    if (!foundTribe) {
+      return res.status(404).json({ error: "Tribe not found!" });
+    }
+    const updatedTribe = await Tribe.updateOne(
+      { _id: foundTribe._id },
+      { ...req.body },
+      { new: true }
+    );
+    return res.json({
+      message: "Tribe updated successfully",
+      avec: updatedTribe,
+    });
+  } catch (error) {
+    return res.status(400).json({ error: "Tribe not updated!", details: error });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const foundTribe = await Tribe.findOne({ _id: req.params.id });
+    if (!foundTribe) {
+      return res.status(404).json({ error: "Tribe not found!" });
+    }
+    const deletedTribe = await Tribe.deleteOne({ _id: foundTribe._id });
+    return res.json({
+      message: "Tribe deleted successfully",
+      avec: deletedTribe,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Tribe not deleted!", details: error });
+  }
+};
