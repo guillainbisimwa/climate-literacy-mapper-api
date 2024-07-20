9f40c3307e77adeea0e540fdfc083a8b9f23f982
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-07-20 18:04:54 +0200

    Add ref model

--- models/ref-model.js
+++ models/ref-model.js
@@ -1,22 +1,9 @@
 const mongoose = require("mongoose");
 
 const refSchema = mongoose.Schema({
-  climate_know_exist: {type: Boolean, required: true },
-  tribe: { type: String},
-  language: { type: String},
-  // What is climate change in your native language
-  climate_change_in_lang: {type: String},
-
-  location: {
-    type: "Point",
-    coordinates: [Number, Number]
-  },
-  proof_link:[{ 
-    name: String,
-    link: String,
-  }],
-  images: { type: [String]},
-
+  email: {type: string },
+  phone: { type: String},
+  
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   timestamp: { type: Date, default: Date.now() },
   status: { type: String, enum: ['PENDING', 'REJECTED', 'ACCEPETED', 'BANNED'], default: 'PENDING'},
