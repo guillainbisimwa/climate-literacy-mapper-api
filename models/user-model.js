f13ac822b2019e206683a84518357eca88cf882e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-12-03 00:20:13 +0200

    Add detailled field to user model

--- models/user-model.js
+++ models/user-model.js
@@ -5,12 +5,15 @@ const userSchema = mongoose.Schema({
   email: { type: String },
   mobile: { type: String },
   password: { type: String },
+  address: { type: String },
+  state: { type: String },
+  city: { type: String },
   role: { type: String },
+  isVerified: { type: Boolean, dafault: false },
   cover_url: { type: String },
   profile_pic: { type: String },
   timestamp: { type: Date, default: Date.now() },
-  
-  status: { type: String, enum: ['PENDING', 'REJECTED', 'ACCEPETED', 'BANNED'], default: 'PENDING'},
+  status: { type: String, enum: ['PENDING', 'REJECTED', 'ACCEPETED', 'BANNED', "DEACTIVATED"], default: 'PENDING'},
 
 });
 
