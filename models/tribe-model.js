b0d285bbca260896c9271c53bd80d041468f9be7
guillainbisimwa <guillainbisimwa@hotmail.fr>
2025-02-04 02:15:19 +0300

    Add other location's type

--- models/tribe-model.js
+++ models/tribe-model.js
@@ -5,7 +5,7 @@ const translate_ = mongoose.Schema({
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   timestamp: { type: Date, default: Date.now() },
   vote: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
-  status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING'},
+  status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING' },
 });
 
 const tribeSchema = new mongoose.Schema({
@@ -15,32 +15,35 @@ const tribeSchema = new mongoose.Schema({
   // What is climate change in your native language
   climate_change_in_language: {
     translate: { type: [translate_], default: [] },
-    status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING'},
+    status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING' },
   },
 
   location: [{
-    radius:Number,
+    radius: Number,
     coordinates: {
       type: [Number],
-    }
+    },
+    name: String,
+    link: String,
+    country: String,
   }],
   proof_link: [{
     name: String,
     link: String,
     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     vote: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
-    status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING'},
+    status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING' },
   }],
   images: [{
-    image: String, 
+    image: String,
     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     vote: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
-    status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING'},
+    status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING' },
   }],
 
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   timestamp: { type: Date, default: Date.now },
-  status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING'},
+  status: { type: String, enum: ['PENDING', 'ACTIVE', 'BANNED'], default: 'PENDING' },
 });
 
 const Tribe = mongoose.model("Tribe", tribeSchema);
